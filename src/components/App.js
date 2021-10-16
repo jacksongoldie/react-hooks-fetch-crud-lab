598d5ca3889279bc6a7c52134fc91cb0034ea4f9
Goldie <goldiejackson25@yahoo.com>
2021-10-16 17:57:58 -0500

    commit tree 542a4167face87e409281d9882b0c6da7261907f

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  function addItem(newQuestion){
+    const updatedQuestions = [...questions, newQuestion]
+    setQuestions(updatedQuestions)
+  }
+
+  function deleteItem(id){
+    const updatedQuestions = questions.filter((question) => question.id !== id)
+    setQuestions(updatedQuestions)
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(resp => resp.json())
+    .then(json => setQuestions(json))
+  }, [])
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddItem={addItem} /> : <QuestionList onDeleteItem={deleteItem} questions={questions}/>}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,35 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteItem }) {
   const { id, prompt, answers, correctIndex } = question;
 
+  console.log(answers)
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+
+  function handleClick(){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+    })
+    .then(r => r.json())
+    .then(() => onDeleteItem(id))
+  }
+
+  function handleChange(e){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "correctIndex": e.target.value
+      })
+    })
+  }
+
 
   return (
     <li>
@@ -15,9 +37,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
